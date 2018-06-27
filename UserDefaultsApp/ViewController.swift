cf5ce3bb2974cbe295a626684bd54c3239a4efc2
Алексей Ефимов <lex.efimov@gmail.com>
2018-06-27 22:34:42 +0500

    Initial Commit

---
+++ UserDefaultsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserDefaultsApp
+//
+//  Created by Alexey Efimov on 27.06.2018.
+//  Copyright © 2018 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
